aeb27d7456f0d9077f0ae2d66781930b01ad5f97
dj221ai <djha5051@gmail.com>
2025-06-24 23:30:13 +0530

    security check

--- backend/worklor/worklor/settings.py
+++ backend/worklor/worklor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)t4i!%euxoaujhuui!f)6=v&u+l9iz+p$=h6t^lr08ih_p)acf'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'worklor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config("DB_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': BASE_DIR / config("db_name", "db.sqlite3"),
     }
 }
 
